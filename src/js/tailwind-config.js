b5633c890823b336c72d6cbbe3621b22f02e7f2a
Lyching Kong <lychingkong@gmail.com>
2026-04-20 11:00:30 +0700

    feat: add tailwind custom configuration

---
+++ src/js/tailwind-config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2C3E50",
+        secondary: "#DFB777",
+        background: "#FBFCF6",
+      },
+    },
+  },
+};
